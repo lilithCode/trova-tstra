5e4839f7c5172814128260b8927fa0db79b2e10b
lilithCode <hamnamubarak.contact@gmail.com>
2025-08-28 03:05:36 +0500

    Optimized Performance

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,9 +56,11 @@ export const metadata = {
   robots: {
     index: true,
     follow: true,
+    nocache: false,
     googleBot: {
       index: true,
       follow: true,
+      noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
